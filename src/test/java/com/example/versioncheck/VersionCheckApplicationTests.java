b521e34043579620126ed8b0b48302c6f93e1b9e
stanislav.savosiuk <stanislav.savosiuk@xenoss.io>
2023-03-09 12:58:47 +0200

    Init

---
+++ src/test/java/com/example/versioncheck/VersionCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.versioncheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
